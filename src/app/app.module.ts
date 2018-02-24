a5610520c17114c44c21677d302fbc5d477640b9
Menno Prinzhorn <menzaa@hotmail.nl>
2018-02-24 14:34:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ResoursesComponent } from './resourses/resourses.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResoursesComponent
   ],
   imports: [
     BrowserModule
