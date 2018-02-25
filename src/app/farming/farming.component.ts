cacfae26545afc0abf173d0e99afe4c61bee9481
Menno Prinzhorn <menzaa@hotmail.nl>
2018-02-25 13:43:10 +0100

    change farming

--- src/app/farming/farming.component.ts
+++ src/app/farming/farming.component.ts
@@ -10,16 +10,21 @@ import { ResourceService } from '../resource.service';
 export class FarmingComponent implements OnInit {
 
 	resources: Resource[];
+
 	constructor(private resourceService: ResourceService) { }
 
 	ngOnInit() {
 		this.resources = this.resourceService.getResources();
+		this.updateResources();
 	}
 
 	incrementResource(resource, amount) {
 		this.resourceService.incrementResource(resource, amount);
-		console.log('test');
 	}
-	//farm.
 
+	updateResources() {
+		setInterval(function(){
+			
+		}, 1000);
+	}
 }
